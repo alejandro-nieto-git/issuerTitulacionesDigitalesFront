01ad1171299eebdb6914aa6b91219871cc1b73ee
Alejandro Nieto <alejandro.nieto.gallego0@gmail.com>
2024-01-23 00:34:30 +0100

    Added navbar. Added home page.

--- app.vue
+++ app.vue
@@ -1,5 +1,29 @@
 <template>
+  <head>
+      <title>Emisor de Titulaciones Digitales</title>
+  </head>
+
+  <ul class="bg-blue-950 mb-2">
+      <li class="text-6xl font-extrabold tracking-tight text-gray-900 dark:text-white text-pretty text-center mb-2"><a
+              href="">Emisor de Titulaciones Digitales</a></li>
+
+      <div class="flex flex-row justify-center items-center">
+          <ULink to="/" active-class="text-primary inline mr-10 font-bold mb-2 hover:text-primary-100" inactive-class="text-primary inline mr-10 font-bold mb-2 hover:text-primary-100">
+              Home 
+          </ULink>
+          <ULink to="/news" active-class="text-primary inline mr-10 font-bold mb-2 hover:text-primary-100" inactive-class="text-primary inline mr-10 font-bold mb-2 hover:text-primary-100">
+              Noticias
+          </ULink>
+          <ULink to="/contact" active-class="text-primary inline mr-10 font-bold mb-2 hover:text-primary-100" inactive-class="text-primary inline mr-10 font-bold mb-2 hover:text-primary-100">
+              Contacto
+          </ULink>
+          <ULink to="/about" active-class="text-primary inline mr-10 font-bold mb-2 hover:text-primary-100" inactive-class="text-primary inline mr-10 font-bold mb-2 hover:text-primary-100">
+              Sobre nosotros 
+          </ULink>
+      </div>
+  </ul>
+
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+
+    <div class="bg-blue-940">
+        <div class="flex flex-col justify-center items-center">
+            <img src="/escudouva.png" alt="Logo Universidad de Valladolid" class="w-96 h-94 mb-28 mt-36">
+            <UButton class="mb-56" icon="i-fa6-regular-address-card" size="xl">Login con EUDIWallet</UButton>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
