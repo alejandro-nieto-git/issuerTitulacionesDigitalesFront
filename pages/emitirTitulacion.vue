240131a4bd285ea2123b2c687714e3d272361571
Alejandro Nieto <alejandro.nieto.gallego0@gmail.com>
2024-03-05 01:41:47 +0100

    Adaptado el estilo de la pagina de emision de la titulacion.

--- pages/emitirTitulacion.vue
+++ pages/emitirTitulacion.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
   const titulacionAEmitir = JSON.parse(localStorage.getItem('titulacionAEmitir')!)
+
+  //TODO: Generar el QR para emitir la titulacion
+  //TODO: Enlazar el boton para emitir la titulacion
 </script>
 
 <template>
-        <div class="flex flex-row justify-center items-center">
         <div class="flex flex-col justify-center items-center text-lg light:text-black text-xl">
-            <p>Se va a emitir tu titulacion {{ titulacionAEmitir.tipo }} en {{ titulacionAEmitir.nombreTitulacion }}</p>
+            <p class="text-4xl mt-10">Se va a emitir tu titulacion "{{ titulacionAEmitir.tipo }} en {{ titulacionAEmitir.nombreTitulacion }}"</p>
 
-            <UButton class="mb-10" icon="i-fa6-regular-address-card" size="xl" @click="selectUserType = true">Emitir </UButton>
+            <UButton class="mb-16 mt-28" icon="i-fa6-regular-address-card" size="xl" @click="emitirTitulacion()">Emitir </UButton>
 
             <UModal v-model="selectUserType">
                 <div class="flex flex-col p-4 justify-center items-center">
@@ -18,7 +20,6 @@
 
             o escanea con tu wallet
 
-            <img src="/QRLoginEUDIWallet.png" alt="QR Login EUDI Wallet" class="w-36 h-34 mb-24 mt-10">
-        </div>
+            <img src="/QRLoginEUDIWallet.png" alt="QR Login EUDI Wallet" class="w-36 h-34 mb-24 mt-16">
         </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -3,11 +3,10 @@
 </script>
 
 <template>
-        <div class="flex flex-row justify-center items-center">
         <div class="flex flex-col justify-center items-center text-lg light:text-black text-xl">
-            <img src="/escudouva.png" alt="Logo Universidad de Valladolid" class="w-96 h-94 mb-20 mt-20">
+            <img src="/escudouva.png" alt="Logo Universidad de Valladolid" class="w-96 h-94 mb-20 mt-16">
 
-            <UButton class="mb-10" icon="i-fa6-regular-address-card" size="xl" @click="selectUserType = true">Login con EUDIWallet</UButton>
+            <UButton class="mb-16" icon="i-fa6-regular-address-card" size="xl" @click="selectUserType = true">Login con EUDIWallet</UButton>
 
             <UModal v-model="selectUserType">
                 <div class="flex flex-col p-4 justify-center items-center">
@@ -18,7 +17,6 @@
 
             o escanea con tu wallet
 
-            <img src="/QRLoginEUDIWallet.png" alt="QR Login EUDI Wallet" class="w-36 h-34 mb-24 mt-10">
-        </div>
+            <img src="/QRLoginEUDIWallet.png" alt="QR Login EUDI Wallet" class="w-36 h-34 mb-24 mt-16">
         </div>
 </template>
