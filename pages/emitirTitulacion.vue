dff5e316abdaf0845dba785c12d7f7679818870f
Alejandro Nieto <alejandro.nieto.gallego0@gmail.com>
2024-03-05 01:29:30 +0100

    Pasada la titulación seleccionada a la pantalla de emisión de la titulación.

---
+++ pages/emitirTitulacion.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  const titulacionAEmitir = JSON.parse(localStorage.getItem('titulacionAEmitir')!)
+</script>
+
+<template>
+        <div class="flex flex-row justify-center items-center">
+        <div class="flex flex-col justify-center items-center text-lg light:text-black text-xl">
+            <p>Se va a emitir tu titulacion {{ titulacionAEmitir.tipo }} en {{ titulacionAEmitir.nombreTitulacion }}</p>
+
+            <UButton class="mb-10" icon="i-fa6-regular-address-card" size="xl" @click="selectUserType = true">Emitir </UButton>
+
+            <UModal v-model="selectUserType">
+                <div class="flex flex-col p-4 justify-center items-center">
+                    <UButton class="mt-4 mb-10 mt-10" icon="i-heroicons-academic-cap-20-solid" size="xl" to="/seleccionarTitulacionAEmitir">Estudiante</UButton>
+                    <UButton class="mb-10" icon="i-pajamas-admin" size="xl" to="/revocarTitulaciones">Encargado</UButton>
+                </div>
+            </UModal>
+
+            o escanea con tu wallet
+
+            <img src="/QRLoginEUDIWallet.png" alt="QR Login EUDI Wallet" class="w-36 h-34 mb-24 mt-10">
+        </div>
+        </div>
+</template>
--- pages/seleccionarTitulacionAEmitir.vue
+++ pages/seleccionarTitulacionAEmitir.vue
@@ -5,78 +5,88 @@ import { ref } from 'vue'
 const listaTitulacionesTerminadas = ref(
   [
     {
-    'nombreTitulacion': 'Ingenieria Industrial',
-    'tipo': 'Grado',
-    'codigoTitulacion': '81639',
-    'notaMedia': '6',
-    'fechaHora': '1709153385',
-    'decretoLey': 'a',
-    'descripcionRegistroFisico': 'b',
+      'nombreTitulacion': 'Ingenieria Industrial',
+      'tipo': 'Grado',
+      'codigoTitulacion': '81639',
+      'notaMedia': '6',
+      'fechaHora': '1709153385',
+      'decretoLey': 'a',
+      'descripcionRegistroFisico': 'b',
     },
     {
-    'nombreTitulacion': 'Filosofia',
-    'tipo': 'Grado',
-    'codigoTitulacion': '82639',
-    'notaMedia': '5.9',
-    'fechaHora': '1709153385',
-    'decretoLey': 'a',
-    'descripcionRegistroFisico': 'b',
+      'nombreTitulacion': 'Filosofia',
+      'tipo': 'Grado',
+      'codigoTitulacion': '82639',
+      'notaMedia': '5.9',
+      'fechaHora': '1709153385',
+      'decretoLey': 'a',
+      'descripcionRegistroFisico': 'b',
     },
     {
-    'nombreTitulacion': 'Ingenieria Informatica',
-    'tipo': 'Grado',
-    'codigoTitulacion': '83639',
-    'notaMedia': '8.6',
-    'fechaHora': '1709153385',
-    'decretoLey': 'a',
-    'descripcionRegistroFisico': 'b',
+      'nombreTitulacion': 'Ingenieria Informatica',
+      'tipo': 'Grado',
+      'codigoTitulacion': '83639',
+      'notaMedia': '8.6',
+      'fechaHora': '1709153385',
+      'decretoLey': 'a',
+      'descripcionRegistroFisico': 'b',
     },
   ]
 )
+
+function storeTitulacionAEmitir(titulacion: any) {
+  localStorage.setItem('titulacionAEmitir', JSON.stringify(titulacion));
+  console.log('Titulacion almacenada en localStorage');
+  console.log(titulacion);
+}
+
 </script>
 
 <template>
   <div class="flex flex-col justify-center items-center mt-10">
     <template v-for="titulacion in listaTitulacionesTerminadas" :key="titulacion.codigoTitulacion">
-    <div class="flex flex-col mb-10 mt-10 size-auto">
-      <UCard :ui="{
-        base: '',
-        background: 'bg-cyan-200',
-        divide: 'divide-y divide-gray-300 dark:divide-gray-800',
-        ring: 'ring-1 ring-gray-300 dark:ring-gray-800',
-        rounded: 'rounded-2xl',
-        shadow: 'shadow-lg',
-        body: {
+      <div class="flex flex-col mb-10 mt-10 size-auto">
+        <UCard :ui="{
           base: '',
-          background: '',
-          padding: 'px-4 py-5 sm:p-6',
-        },
-        header: {
-          base: '',
-          background: '',
-          padding: 'px-4 py-5 sm:px-6',
-        },
-        footer: {
-          base: '',
-          background: '',
-          padding: 'px-4 py-4 sm:px-6',
-        },
-      }">
-        <template #header>
-          <div class="text-black text-xl font-bold">
-            {{titulacion.nombreTitulacion}}
+          background: 'bg-cyan-200',
+          divide: 'divide-y divide-gray-300 dark:divide-gray-800',
+          ring: 'ring-1 ring-gray-300 dark:ring-gray-800',
+          rounded: 'rounded-2xl',
+          shadow: 'shadow-lg',
+          body: {
+            base: '',
+            background: '',
+            padding: 'px-4 py-5 sm:p-6',
+          },
+          header: {
+            base: '',
+            background: '',
+            padding: 'px-4 py-5 sm:px-6',
+          },
+          footer: {
+            base: '',
+            background: '',
+            padding: 'px-4 py-4 sm:px-6',
+          },
+        }">
+          <template #header>
+            <div class="text-black text-xl font-bold">
+              {{ titulacion.nombreTitulacion }}
+            </div>
+          </template>
+
+          <div class="flex flex-row">
+            Tipo: {{ titulacion.tipo }} <br>
+            Codigo titulacion: {{ titulacion.codigoTitulacion }} <br>
+            Nota media: {{ titulacion.notaMedia }} <br>
+            <UButton class="mt-4 ml-96 self-center" icon="i-heroicons-academic-cap-20-solid" size="xl"
+              to="/emitirTitulacion" @click="storeTitulacionAEmitir(titulacion)">Solicitar emision</UButton>
+
+
           </div>
-        </template>
+        </UCard>
+      </div>
+    </template>
 
-        <div class="flex flex-row">
-            Tipo: {{titulacion.tipo}} <br>
-            Codigo titulacion: {{titulacion.codigoTitulacion}} <br>
-            Nota media: {{titulacion.notaMedia}} <br>
-            <UButton class="mt-4 ml-96 self-center" icon="i-heroicons-academic-cap-20-solid" size="xl" to="/emitirTitulacion">Emitir</UButton>
-        </div>
-      </UCard>
-    </div>
-  </template>
-
-  </div> 
+  </div>
 </template>
