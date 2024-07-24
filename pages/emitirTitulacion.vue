ccb6abbf63d1ffc26e5c8e5caf96642c5ff0c6a6
alejandro.nieto <alejandro.nieto.gallego0@gmail.com>
2024-07-24 07:50:07 +0200

    Deleted user type selection when emitting Titulacion Digital.

--- pages/emitirTitulacion.vue
+++ pages/emitirTitulacion.vue
@@ -11,13 +11,6 @@
 
             <UButton class="mb-16 mt-28" icon="i-fa6-regular-address-card" size="xl" @click="emitirTitulacion()">Emitir </UButton>
 
-            <UModal v-model="selectUserType">
-                <div class="flex flex-col p-4 justify-center items-center">
-                    <UButton class="mt-4 mb-10 mt-10" icon="i-heroicons-academic-cap-20-solid" size="xl" to="/seleccionarTitulacionAEmitir">Estudiante</UButton>
-                    <UButton class="mb-10" icon="i-pajamas-admin" size="xl" to="/revocarTitulaciones">Encargado</UButton>
-                </div>
-            </UModal>
-
             o escanea con tu wallet
 
             <img src="/QRLoginEUDIWallet.png" alt="QR Login EUDI Wallet" class="w-36 h-34 mb-24 mt-16">
