748efe25a277fffcc35dbfa52b66cfc9049d8030
alejandro.nieto <alejandro.nieto.gallego0@gmail.com>
2024-09-17 00:12:53 +0200

    Extracted model of titulacion. Added preprocessing of titulaciones to be able to display them properly. Added titulacion code to the CRUD. Connected to the DB to retrieve issued titulaciones. Connected to the DB to revoke titulaciones.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,11 +3,13 @@ export default defineNuxtConfig({
   colorMode: {
     preference: 'light'
   },
+
   devtools: { enabled: true },
   modules: ['@nuxt/ui', 'nuxt-icon'],
+
   ui: {
     icons: ['fa6-regular', 'ph', 'pajamas']
-  }
+  },
 
+  compatibilityDate: '2024-09-14'
 })
-
--- pages/revocarTitulaciones.vue
+++ pages/revocarTitulaciones.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="issued-titulaciones-manager">
-    <UTable :rows="titulacionCredentials" :columns="fields">
+    <UTable :rows="preProcessedTitulacionCredentials" :columns="fields">
       <template #credentialSubject.hasTitulacion.revocada-data="{ row }">
         <div v-if="row.credentialSubject.hasTitulacion.revocada">
           <span>Revocada</span>
@@ -27,40 +27,16 @@
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
+import type { TitulacionCredential } from "../titulacion-digital-model/src/model/titulacion";
+import axios from 'axios';
 
-interface HasTitulacion {
-  codigoTitulacion: string;
-  nombreTitulacion: string;
-  tipo: string;
-  promocion: string;
-  notaMedia: string;
-  fechaHoraEmision: string;
-  revocada: boolean;
-  revoking?: boolean;
-  decretoLey: string;
-  descripcionRegistroFisico: string;
-}
 
-interface CredentialSubject {
-  id: string;
-  nif: string;
-  nombre: string;
-  apellido1: string;
-  apellido2: string;
-  fechaNacimiento: string;
-  hasTitulacion: HasTitulacion;
-}
-
-interface TitulacionCredential {
-  type: string[];
-  issuer: string;
-  issuanceDate: string;
-  credentialSubject: CredentialSubject;
-}
 
 const titulacionCredentials = ref<TitulacionCredential[]>([]);
+let preProcessedTitulacionCredentials = ref<any[]>([]);
 
 const fields = [
+  { key: 'credentialSubject.hasTitulacion.codigoTitulacion', label: 'Código titulacion', sortable: false },
   { key: 'credentialSubject.nombre', label: 'Nombre' },
   { key: 'credentialSubject.apellido1', label: 'Primer Apellido' },
   { key: 'credentialSubject.apellido2', label: 'Segundo Apellido' },
@@ -70,86 +46,52 @@ const fields = [
 ];
 
 async function fetchTitulacionCredentials() {
-  //TODO: get issued titulaciones from the database by calling an issuer endpoint
-  const rawData = [
-    {
-      credential: {
-        "@context": ["https://www.w3.org/2018/credentials/v1"],
-        "type": ["TitulacionDigital"],
-        "issuer": "did:ethr:89bd0B47C4302435aA1bb2f621a447c34420B195",
-        "issuanceDate": "2024-08-17T22:01:26.996Z",
-        "credentialSubject": {
-          "nif": { "display": [{ "nif": "12345678A" }] },
-          "nombre": { "display": [{ "nombre": "Juan" }] },
-          "apellido1": { "display": [{ "apellido1": "Perez" }] },
-          "apellido2": { "display": [{ "apellido2": "Gomez" }] },
-          "fechaNacimiento": { "display": [{ "fechaNacimiento": "1990-01-01" }] },
-          "hasTitulacion": {
-            "display": [{
-              "codigoTitulacion": "83639",
-              "nombreTitulacion": "Ingeniería Informática",
-              "tipo": "Grado",
-              "promocion": "2017",
-              "notaMedia": "8.6",
-              "fechaHoraEmision": "2021-07-12T12:00:00Z",
-              "revocada": false,
-              "decretoLey": "Real Decreto 123/2017",
-              "descripcionRegistroFisico": "Registro Físico"
-            }]
-          },
-          "id": "did:ethr:DE19d461d3E3Fc360D392B512fa09aBcB6A3cba3"
-        }
-      }
-    }
-  ];
-
-  titulacionCredentials.value = rawData.map(item => ({
-      type: item.credential.type,
-      issuer: item.credential.issuer,
-      issuanceDate: item.credential.issuanceDate,
-      credentialSubject: {
-        id: item.credential.credentialSubject.id,
-        nif: item.credential.credentialSubject.nif.display[0].nif,
-        nombre: item.credential.credentialSubject.nombre.display[0].nombre,
-        apellido1: item.credential.credentialSubject.apellido1.display[0].apellido1,
-        apellido2: item.credential.credentialSubject.apellido2.display[0].apellido2,
-        fechaNacimiento: item.credential.credentialSubject.fechaNacimiento.display[0].fechaNacimiento,
-        hasTitulacion: {
-          codigoTitulacion: item.credential.credentialSubject.hasTitulacion.display[0].codigoTitulacion,
-          nombreTitulacion: item.credential.credentialSubject.hasTitulacion.display[0].nombreTitulacion,
-          tipo: item.credential.credentialSubject.hasTitulacion.display[0].tipo,
-          promocion: item.credential.credentialSubject.hasTitulacion.display[0].promocion,
-          notaMedia: item.credential.credentialSubject.hasTitulacion.display[0].notaMedia,
-          fechaHoraEmision: item.credential.credentialSubject.hasTitulacion.display[0].fechaHoraEmision,
-          revocada: item.credential.credentialSubject.hasTitulacion.display[0].revocada,
-          decretoLey: item.credential.credentialSubject.hasTitulacion.display[0].decretoLey,
-          descripcionRegistroFisico: item.credential.credentialSubject.hasTitulacion.display[0].descripcionRegistroFisico,
-        }
-      }
-  }));
+  titulacionCredentials.value = (await axios.get(import.meta.env.VITE_ISSUER_URI + "/titulacion")).data;
+  preProcessedTitulacionCredentials.value = preprocessTitulacionCredentials(titulacionCredentials.value);
   console.log("Fetch titulaciones", titulacionCredentials.value);
 }
 
-function toggleRevocation(titulacion: TitulacionCredential) {
+function toggleRevocation(titulacion: any) {
   titulacion.credentialSubject.hasTitulacion.revoking = !titulacion.credentialSubject.hasTitulacion.revoking;
   console.log("Revoking switched to", titulacion.credentialSubject.hasTitulacion.revoking);
 }
 
 async function saveTitulacionCredential(titulacion: TitulacionCredential) {
-  // TODO: Save the updated titulacion to the database by calling an issuer endpoint
+  let titulacionId = titulacion._id;
+  delete titulacion._id;
+  await axios.put(import.meta.env.VITE_ISSUER_URI + "/titulacion/" + titulacionId, titulacion);
   console.log("Titulacion saved", titulacion);
 }
 
-function confirmRevoke(titulacion: TitulacionCredential) {
+async function confirmRevoke(titulacion: any) {
   const nombre = titulacion.credentialSubject.nombre;
   const apellido1 = titulacion.credentialSubject.apellido1;
   if (confirm(`¿Está seguro que desea revocar la titulación para ${nombre} ${apellido1}?`)) {
-    titulacion.credentialSubject.hasTitulacion.revocada = true;
-    console.log("Revoke confirmed", titulacion);
-    delete titulacion.credentialSubject.hasTitulacion.revoking;
-    saveTitulacionCredential(titulacion);
+    let revokedTitulacion = (await axios.get(import.meta.env.VITE_ISSUER_URI + "/titulacion/" + titulacion._id)).data;
+    console.log("Revoking", revokedTitulacion);
+    revokedTitulacion.credentialSubject.hasTitulacion.display[0].revocada = true;
+    console.log("Revoke confirmed", revokedTitulacion);
+    saveTitulacionCredential(revokedTitulacion);
   }
 }
+
+function preprocessTitulacionCredentials(credentials: TitulacionCredential[]) {
+  return credentials.map(credential => ({
+    ...credential,
+    credentialSubject: {
+      ...credential.credentialSubject,
+      nif: credential.credentialSubject.nif.display[0].nif,
+      nombre: credential.credentialSubject.nombre.display[0].nombre,
+      apellido1: credential.credentialSubject.apellido1.display[0].apellido1,
+      apellido2: credential.credentialSubject.apellido2.display[0].apellido2,
+      fechaNacimiento: credential.credentialSubject.fechaNacimiento.display[0].fechaNacimiento,
+      hasTitulacion: {
+        ...credential.credentialSubject.hasTitulacion.display[0],
+      },
+    },
+  }));
+}
+
 
 onMounted(fetchTitulacionCredentials);
 </script>
